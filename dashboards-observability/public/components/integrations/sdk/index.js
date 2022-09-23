efd47d37fe71add6de9b52f24eea883eadb15e04
Abhay Pandey <abhay_pandey@persistent.com>
2022-09-23 10:45:42 +0530

    template check
    Signed-off-by: Abhay Pandey <abhay_pandey@persistent.com>

--- dashboards-observability/public/components/integrations/sdk/index.js
+++ dashboards-observability/public/components/integrations/sdk/index.js
@@ -1,31 +1,50 @@
 // const prompt = require('prompt');
 const { mkdir } = require('fs').promises;
 var fs = require('fs');
+var path = require('path');
 const indexTemplate = require('./templates/indexTemplate');
 
 const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+const prompt = (query) => new Promise((resolve) => rl.question(query, resolve));
 
-rl.question('User Input', function (name) {
-    console.log(name);
-  rl.question('Where do you live ? ', function (country) {
-    console.log(`${name}, is a citizen of ${country}`);
+(async() => {
+  try {
+    const name = await prompt("Action Name: ");
+
+    var actionName = name.split(" ");
+    switch (actionName[0]) {
+      case 'nginx':
+        createFileStructure(actionName[0]);
+        break;
+      
+      case 'apache':
+        createFileStructure(actionName[0]);
+        break;
+        
+      case 'clean':
+        cleanFolder(path.join(__dirname, `../plugins/${actionName[1]}`));
+        break;  
+    
+      default:
+        console.log("Please Provide Appropriate input");
+        break;
+    }
+    // const lastName = await prompt(`Thanks for the test`);
     rl.close();
-  });
-});
+  } catch (e) {
+    console.error("Unable to prompt", e);
+  }
+})();
 
-rl.on('close', function () {
-  console.log('\nBYE BYE !!!');
-  process.exit(0);
-});
+// When done reading prompt, exit program 
+rl.on('close', () => process.exit(0));
 
-async function createFileStructure(file){
+async function createFileStructure(fi){
+  let file = path.join(__dirname, `../plugins/${fi}`)
     if (!fs.existsSync(file)){
         fs.mkdirSync(file, { recursive: true });
-        fs.writeFile(`${file}/${file}.tsx`, indexTemplate.content.replace(/{file}/gi,file[0].toUpperCase() + file.slice(1)), function (err) {
+        fs.writeFile(`${file}/index.tsx`, indexTemplate.content.replace(/{file}/gi,fi[0].toUpperCase() + fi.slice(1)), function (err) {
           if (err) throw err;
         });
         const compl = await createSubFolder(file);
@@ -44,52 +63,17 @@ async function createFilesInFolder(dirName,parent){
       if (err) throw err;
     });
   })
-}
-
-// prompt.start();
-
-// prompt.get(['integerationName'], function (err, result) {
-//   if (err) {
-//     return onErr(err);
-//   }
-//   console.log('Command-line input received:');
-//   console.log('  Integeration Name : ' + result.integerationName);
-
-//   var actionName = result.integerationName.split(" ");
-
-//   // console.log(test.split(" "));
-
-//   console.log(actionName[0]);
-//   switch (actionName[0]) {
-//     case 'nginx':
-//       createFileStructure(actionName[0]);
-//       break;
-    
-//     case 'apache':
-//       createFileStructure(actionName[0]);
-//       break;
-      
-//     case 'clean':
-//       cleanFolder(actionName[1]);
-//       // createFileStructure(result.integerationName);
-//       break;  
-  
-//     default:
-//       console.log("Please Provide Appropriate input");
-//       break;
-//   }
-// });
+};
 
 var folderFile = {
   doc :["constant.tsx", "index.tsx"],
-  schema :["schema.ts"],
-  tabs: ["index.tsx"]
+  schema :["schema.ts"]
+  // tabs: ["index.tsx"]
 };
 
 async function createSubFolder(parent) {
   try {
-    const dirnames  = ['doc', 'schema', 'tabs'];
-
+    const dirnames  = ['doc', 'schema'];
     await Promise.all(
       dirnames.map(dirname => mkdir(`${parent}/${dirname}`)
       .then((res) => {
@@ -100,7 +84,7 @@ async function createSubFolder(parent) {
           console.log(fs.existsSync(`${parent}/${dirname}`));
         }
       })
-      .catch(console.error))
+      .catch((err) => {console.error(err)}))
     );
     // All dirs are created here or errors reported.
   } catch (err) {
