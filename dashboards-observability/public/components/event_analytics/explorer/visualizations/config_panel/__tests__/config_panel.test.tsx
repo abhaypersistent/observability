129e78c47368da6f14c478a5893a7c08f8cde9a5
Abhay Pandey <abhay_pandey@persistent.com>
2022-11-15 11:53:57 +0000

    final code
    Signed-off-by: Abhay Pandey <abhay_pandey@persistent.com>

--- dashboards-observability/public/components/event_analytics/explorer/visualizations/config_panel/__tests__/config_panel.test.tsx
+++ dashboards-observability/public/components/event_analytics/explorer/visualizations/config_panel/__tests__/config_panel.test.tsx
@@ -8,12 +8,9 @@ import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
 import { waitFor } from '@testing-library/react';
 import { ConfigPanel } from '../config_panel';
-import { ConfigGaugeValueOptions } from '../config_panes/config_controls/config_gauge_options';
-import { ConfigPanelOptionGauge } from '../config_panes/config_controls/config_panel_option_gauge';
 import {
   TEST_VISUALIZATIONS_DATA,
   EXPLORER_VISUALIZATIONS,
-  GAUGE_TEST_VISUALIZATIONS_DATA,
 } from '../../../../../../../test/event_analytics_constants';
 import { TabContext } from '../../../../hooks';
 import PPLService from '../../../../../../services/requests/ppl';
@@ -28,7 +25,6 @@ describe('Config panel component', () => {
     const setCurVisId = jest.fn();
     const tabId = 'query-panel-1';
     const curVisId = 'bar';
-    const curVisIdG = 'gauge';
     const pplService = new PPLService(httpClientMock);
     const mockChangeIsValidConfigOptionState = jest.fn();
 
@@ -52,65 +48,10 @@ describe('Config panel component', () => {
       </TabContext.Provider>
     );
 
-    const gaugeWrapper = mount(
-      <TabContext.Provider
-        value={{
-          tabId,
-          curVisIdG,
-          dispatch: jest.fn(),
-          changeVisualizationConfig: jest.fn(),
-          explorerVisualizations: EXPLORER_VISUALIZATIONS,
-          setToast: jest.fn(),
-          pplService,
-        }}
-      >
-        <ConfigGaugeValueOptions
-          visualizations={TEST_VISUALIZATIONS_DATA}
-          schemas={[]}
-          sectionName={''}
-          vizState={''}
-          handleConfigChange={mockChangeIsValidConfigOptionState}
-        />
-      </TabContext.Provider>
-    );
-
-    const gaugeWrapperOption = mount(
-      <TabContext.Provider
-        value={{
-          tabId,
-          curVisIdG,
-          dispatch: jest.fn(),
-          changeVisualizationConfig: jest.fn(),
-          explorerVisualizations: EXPLORER_VISUALIZATIONS,
-          setToast: jest.fn(),
-          pplService,
-        }}
-      >
-        <ConfigPanelOptionGauge
-          visualizations={TEST_VISUALIZATIONS_DATA}
-          panelOptionsValues={''}
-          vizState={''}
-          handleConfigChange={mockChangeIsValidConfigOptionState}
-        />
-      </TabContext.Provider>
-    );
-
     wrapper.update();
 
     await waitFor(() => {
       expect(wrapper).toMatchSnapshot();
-    });
-
-    gaugeWrapper.update();
-
-    await waitFor(() => {
-      expect(gaugeWrapper).toMatchSnapshot();
-    });
-
-    gaugeWrapperOption.update();
-
-    await waitFor(() => {
-      expect(gaugeWrapperOption).toMatchSnapshot();
     });
   });
 });
